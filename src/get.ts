c4819f3d7b92748dc7b58209450906b0103656e5
Ryan Sonshine <ryansonshine@users.noreply.github.com>
2021-05-04 11:10:08 -0400

    feat(types): add type definitions to handlers

--- src/get.ts
+++ src/get.ts
@@ -1,50 +1,53 @@
-import { DynamoDB } from 'aws-sdk'
+import { APIGatewayEvent, APIGatewayEventRequestContext, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
 
-const ddb = new DynamoDB.DocumentClient()
+const ddb = new DynamoDB.DocumentClient();
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {APIGatewayEvent} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {APIGatewayEventRequestContext} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ * @returns {APIGatewayProxyResult} object - API Gateway Lambda Proxy Output Format
+ *
  */
-export const handler = async (event: any = {}): Promise<any> => {
-    let response
-    try {
-      const params = {
-        TableName: 'data-table',
-        Key: {
-          data: event.pathParameters.data
-        }
-      }
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  let response: APIGatewayProxyResult;
+  try {
+    const params = {
+      TableName: 'data-table',
+      Key: {
+        data: event.pathParameters?.data,
+      },
+    };
 
-      const results = await ddb.get(params).promise()
+    const results = await ddb.get(params).promise();
 
-      if (!results || !results.Item) {
-        response = {
-          headers: {'Access-Control-Allow-Origin': '*'},
-          statusCode: 400,
-        }
-      } else {
-        response = {
-          headers: {'Access-Control-Allow-Origin': '*'},
-          statusCode: 200,
-          body: JSON.stringify(results.Item),
-        }
-      }
-    } catch (err) {
-      console.log(err)
+    if (!results || !results.Item) {
       response = {
-        headers: {'Access-Control-Allow-Origin': '*'},
-        statusCode: 500,
-      }
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        statusCode: 400,
+        body: 'No results',
+      };
+    } else {
+      response = {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        statusCode: 200,
+        body: JSON.stringify(results.Item),
+      };
     }
+  } catch (err) {
+    console.log(err);
+    response = {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 500,
+      body: err.message || 'Internal server error',
+    };
+  }
 
-    return response
-}
+  return response;
+};
--- src/simple.ts
+++ src/simple.ts
@@ -1,29 +1,31 @@
-let response;
+import { APIGatewayEvent, APIGatewayEventRequestContext, APIGatewayProxyResult } from 'aws-lambda';
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {APIGatewayEvent} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {APIGatewayEventRequestContext} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ * @returns {APIGatewayProxyResult} object - API Gateway Lambda Proxy Output Format
+ *
  */
-export const handler = async (event: any = {}): Promise<any> => {
-    try {
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello simple world!',
-            })
-        }
-    } catch (err) {
-        console.log(err)
-        return err
-    }
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  let response: APIGatewayProxyResult;
 
-    return response
-}
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello simple world!',
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
+};
